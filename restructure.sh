7a9fa30c74b0676a20a5a78825dcdd97d30fed9b
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2023-10-04 17:42:36 +0200

    ...

--- restructure.sh
+++ restructure.sh
@@ -5,13 +5,14 @@ mkdir pylib/python_lib
 mkdir output/dist_lib
 cp -rf kivy_build/dist/xcframework output/
 cp -rf kivy_build/dist/lib/* pylib/dist_lib
-cp -rf kivy_build/dist/root/python3/lib/* pylib/python_lib
-
+#cp -rf kivy_build/dist/root/python3/lib/* pylib/python_lib
+zip -r python_lib.zip kivy_build/dist/root/python3/lib/*
 
 cp -rf kivy_build/dist/root/python3/include/python3.10 output/xcframework/libpython3.10.xcframework/ios-arm64/
 cp -rf kivy_build/dist/root/python3/include/python3.10 output/xcframework/libpython3.10.xcframework/ios-x86_64-simulator/
 cp -f PythonXc_Info.plist output/xcframework/libpython3.10.xcframework/Info.plist
 cp -f create_package.sh output/
+cp -f python_lib.sh output/
 
 
 cd output/xcframework
