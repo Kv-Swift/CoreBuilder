24229f402db7a0ea0b02e579ee5c2a9258a8c80c
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2023-10-04 18:36:13 +0200

    ...

--- restructure.sh
+++ restructure.sh
@@ -1,18 +1,20 @@
 
 mkdir output
 mkdir pylib
-mkdir pylib/python_lib
-mkdir output/dist_lib
+#mkdir pylib/python_lib
+#mkdir output/dist_lib
 cp -rf kivy_build/dist/xcframework output/
-cp -rf kivy_build/dist/lib/* output/dist_lib
+#cp -rf kivy_build/dist/lib/* output/dist_lib
 #cp -rf kivy_build/dist/root/python3/lib/* pylib/python_lib
 zip -r python_lib.zip kivy_build/dist/root/python3/lib/*
+zip -r dist_lib.zip kivy_build/dist/lib/*
 
-cp -rf kivy_build/dist/root/python3/include/python3.10 output/xcframework/libpython3.10.xcframework/ios-arm64/
-cp -rf kivy_build/dist/root/python3/include/python3.10 output/xcframework/libpython3.10.xcframework/ios-x86_64-simulator/
-cp -f PythonXc_Info.plist output/xcframework/libpython3.10.xcframework/Info.plist
-cp -f create_package.sh output/
-cp -f python_lib.zip output/
+mv -f kivy_build/dist/root/python3/include/python3.10 output/xcframework/libpython3.10.xcframework/ios-arm64/
+mv -f kivy_build/dist/root/python3/include/python3.10 output/xcframework/libpython3.10.xcframework/ios-x86_64-simulator/
+mv -f PythonXc_Info.plist output/xcframework/libpython3.10.xcframework/Info.plist
+mv -f create_package.sh output/
+mv -f python_lib.zip output/
+mv -f dist_lib.zip output/
 
 
 cd output/xcframework
