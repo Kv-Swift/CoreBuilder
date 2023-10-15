b3aee521d4a0b4af0c358358f745e267fb256911
psychowasp <psychowaspx@gmail.com>
2023-10-15 03:35:03 +0200

    ...

--- package_templates/KivyPythonCore/Package.swift
+++ package_templates/KivyPythonCore/Package.swift
@@ -11,19 +11,15 @@ let package = Package(
 			name: "PythonCore",
 			targets: [
 				"PythonCore",
-				"libpython3.10", "libssl", "libcrypto", "libffi",
+				"libpython310", "libssl", "libcrypto", "libffi",
 			]
 		),
     ],
     targets: [
-		.binaryTarget(name: "libcrypto", path: "Resources/xcframework/libcrypto.zip"),
-		.binaryTarget(name: "libpython3.10", path: "Resources/xcframework/libpython3.10.zip"),
-		.binaryTarget(name: "libffi", path: "Resources/xcframework/libffi.zip"),
-		.binaryTarget(name: "libssl", path: "Resources/xcframework/libssl.zip"),
 		.target(
 			name: "PythonCore",
 			dependencies: [
-				"libpython3.10",
+				"libpython310",
 				"libssl",
 				"libcrypto",
 				"libffi",
